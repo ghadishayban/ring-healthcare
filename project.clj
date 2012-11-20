13e55d49244705edbbd9fb238baa7bf6559c366f
Ghadi Shayban <gshayban@gmail.com>
2012-11-20 14:31:37 -0500

    working tcp endpoint

--- project.clj
+++ project.clj
@@ -5,6 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]
                  [org.apache.camel/camel-core "2.10.2"]
-                 [org.apache.camel/camel-hl7 "2.10.2"]]
+                 [org.apache.camel/camel-hl7 "2.10.2"]
+                 [org.apache.camel/camel-mina2 "2.10.2"] 
+                 [org.apache.mina/mina-core "2.0.7"]]  
   :repositories [["sonatype" {:snapshots true
                               :url "https://oss.sonatype.org/content/groups/public/"}]])
--- src/com/shayban/ring_mllp_adapter/endpoint.clj
+++ src/com/shayban/ring_mllp_adapter/endpoint.clj
@@ -1,1 +1,35 @@
-(ns com.shayban.ring-mllp-adapter.endpoint)
+(ns com.shayban.ring-mllp-adapter.endpoint
+  (:import [org.apache.camel Processor Exchange]
+           [org.apache.camel.builder RouteBuilder]
+           [org.apache.camel.impl SimpleRegistry DefaultCamelContext]
+           org.apache.camel.component.hl7.HL7MLLPCodec))
+
+(defn- proxy-processor
+  [handler]
+  (proxy [Processor] []
+    (process [^Exchange excg]
+      (let [input (.. excg .getIn (.getBody String))
+            output (handler input)] 
+        (.. excg .getOut (.setBody output))))))
+
+(defn- build-url [port]
+  (format "mina2:tcp://0.0.0.0:%s?sync=true&codec=#hl7mllp" port))
+
+(defn- route-builder [url handler]
+  (proxy [RouteBuilder] []
+    (configure []
+      (.. this
+        (from url)
+        (process (proxy-processor handler))))))
+
+(defn- start-camel-context [routebuilder]
+  (let [r (SimpleRegistry.)
+        ctx (doto (DefaultCamelContext. r))]
+    (.put r "hl7mllp" (HL7MLLPCodec.))
+    (.addRoutes ctx  routebuilder)
+    (.start ctx)
+    ctx))
+
+(defn run-listener [handler {:keys [port]}]
+  (let [rb (route-builder (build-url port) handler)]
+    (start-camel-context rb)))
